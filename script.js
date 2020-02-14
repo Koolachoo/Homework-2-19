57435a13b59a2d8958e92e69b61600b625c45762
Justin Taylor <kiradez211@gmail.com>
2020-02-14 00:52:43 -0500

    Added timer

---
+++ script.js
@@ -0,0 +1,23 @@
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 60;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+
+    setTime();
+
+    
+    
