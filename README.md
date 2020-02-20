2c52615c4c4ca71eeef4458667cf93f41756737d
Koolachoo <59377249+Koolachoo@users.noreply.github.com>
2020-02-20 01:31:22 -0500

    Final Changes

---
+++ README.md
@@ -0,0 +1,8 @@
+# URL
+
+# Quiz 
+- Designed to test your knowledge of coding
+- Upon clicking the start button, four questions and the timer are generated
+- When the question is answered, the score goes up or the timer loses 20 seconds
+- When timer reaches zero, or two questions are answered, the game is over
+- The user can submit the initials and score that was announced
--- script.js
+++ script.js
@@ -1,23 +1,109 @@
+// variables 
 var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
-
+var questionElement = document.getElementById("question");
+var startButton = document.getElementById("start-button");
+var questionBox = document.getElementById("ques-box");
+var buttonsElement = document.getElementById("buttons");
+var buttonOne = document.getElementById("btn0");
+var buttonTwo = document.getElementById("btn1");
+var buttonThree = document.getElementById("btn2");
+var buttonFour = document.getElementById("btn3");
+var userInitials = document.getElementById("initials");
+var userScore = document.getElementById("score");
+var enter = document.getElementById("enter");
+var card = document.querySelector(".card");
+var clicks = 0;
+var score = 0;
 var secondsLeft = 60;
 
+//hides the questions and form
+card.style.visibility = "hidden";
+questionBox.style.visibility = "hidden";
+
+
+startButton.addEventListener("click", beginGame)
+
+function beginGame() {
+    setTime();
+    startButton.style.display = "none";
+    questionBox.style.visibility = "visible";
+    answerSet();
+}
+
+function answerSet() {
+    questionElement.textContent = "CSS stands for: "
+    buttonOne.textContent = "Cascading Style Sheet";
+    buttonTwo.textContent = "Chocolate Sprinkle Sundae";
+    buttonThree.textContent = "Carry Sally Sanders";
+    buttonFour.textContent = "Coding Startup Section";
+}
+
+//replaces first set of questions with second set
+function swap() {
+    questionElement.textContent = "Javascript is the: "
+    buttonOne.textContent = "programming language of HTML and the Web";
+    buttonTwo.textContent = "the easiest way to make coffee";
+    buttonThree.textContent = "the language of beans";
+    buttonFour.textContent = "hypertext transfer protocol";
+}
+
+//buttons 
+buttonOne.addEventListener("click", function () {
+    score++;
+    clicks++;
+    swap();
+});
+buttonTwo.addEventListener("click", function () {
+    secondsLeft -= 20;
+    clicks++;
+    swap();
+});
+buttonThree.addEventListener("click", function () {
+    secondsLeft -= 20;
+    clicks++;
+    swap();
+
+
+});
+buttonFour.addEventListener("click", function () {
+    secondsLeft -= 20;
+    clicks++;
+    swap();
+});
+
+// saves the score and initials
+enter.addEventListener("click", function (event) {
+    event.preventDefault();
+
+var initialsValue = document.querySelector("#initials").value;
+var scoreValue = document.querySelector("#score").value;
+
+   
+    localStorage.setItem("initialsValue", initialsValue);
+    localStorage.setItem("scoreValue", scoreValue);
+});
+
+
+
 function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left";
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left";
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
+        if (secondsLeft === 0 || clicks === 2) {
+            clearInterval(timerInterval);
+            alert("Your final score is " + score +"!");
+            questionBox.style.display = "none";
+            card.style.visibility = "visible";
+        
+        }
 
-  }, 1000);
+    }, 1000);
 }
 
 
-    setTime();
 
-    
-    
+
+
+
+
